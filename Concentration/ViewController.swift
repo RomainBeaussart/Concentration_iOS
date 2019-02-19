8651d3e481f9aabc647a7b86a655ad1fb9c012ad
Romain Beaussart <romain.beaussart@efrei.net>
2019-02-19 17:16:53 +0100

    first commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Romain BEAUSSART on 19/02/2019.
+//  Copyright © 2019 Romain BEAUSSART. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var flipCount = 0 {
+        didSet{
+            flipCountLabel.text = "Flips : \(flipCount)"
+        }
+    }
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    var emojiChoices = ["😇","😂","😇","😂"]
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.index(of: sender){
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        } else {
+            print("chosen card was not in cardButtons")
+        }
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+        }
+    }
+}
+
+
